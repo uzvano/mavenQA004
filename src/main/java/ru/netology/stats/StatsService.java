bf28f87fb2c97af7b8cb06dd42f9fc28350e5be4
Ivan <uzvano@mail.ru>
2024-07-17 19:31:51 +0300

    Changed Test

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -2,56 +2,58 @@ package ru.netology.stats;
 
 public class StatsService {
 
-    public long sumSales(long[] arr) {
+    public long sumSales(long[] sales) {
         long sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
+        for (int i = 0; i < sales.length; i++) {
+            sum = sum + sales[i];
         }
         return sum;
     }
 
-    public long sumSalesAverage(long[] arr) {
+    public long sumSalesAverage(long[] sales) {
         long sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
+        for (int i = 0; i < sales.length; i++) {
+            sum = sum + sales[i];
         }
         return sum / 12;
     }
 
-    public int maxSales(long[] arr) {
+    public int maxSales(long[] sales) {
         int maxMonth = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >= arr[maxMonth]) {
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
                 maxMonth = i;
             }
         }
         return maxMonth + 1;
     }
 
-    public int minSales(long[] arr) {
+    public int minSales(long[] sales) {
         int minMonth = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] <= arr[minMonth]) {
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) {
                 minMonth = i;
             }
         }
         return minMonth + 1;
     }
 
-    public int getMonthsSalesLowAverage(long[] arr) {
+    public int getMonthsSalesLowAverage(long[] sales) {
         int monthsLow = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < sumSalesAverage(arr)) {
+        long salesAverage = sumSalesAverage(sales);
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < salesAverage) {
                 monthsLow += 1;
             }
         }
         return monthsLow;
     }
 
-    public int getMonthsSalesHigherAverage(long[] arr) {
+    public int getMonthsSalesHigherAverage(long[] sales) {
         int monthsHigher = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > sumSalesAverage(arr)) {
+        long salesAverage = sumSalesAverage(sales);
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > salesAverage) {
                 monthsHigher += 1;
             }
         }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -5,48 +5,48 @@ import org.junit.jupiter.api.Test;
 
 public class StatsServiceTest {
 
-    long[] arr = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
     StatsService service = new StatsService();
 
     @Test
     public void testSumSales() {
         long expected = 180;
-        long actual = service.sumSales(arr);
+        long actual = service.sumSales(sales);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testSumSalesAverage() {
         long expected = 15;
-        long actual = service.sumSalesAverage(arr);
+        long actual = service.sumSalesAverage(sales);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testMaxSales() {
         int expected = 8;
-        int actual = service.maxSales(arr);
+        int actual = service.maxSales(sales);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testMinSales() {
         int expected = 9;
-        int actual = service.minSales(arr);
+        int actual = service.minSales(sales);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testGetMonthsSalesLowAverage() {
         int expected = 5;
-        int actual = service.getMonthsSalesLowAverage(arr);
+        int actual = service.getMonthsSalesLowAverage(sales);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testGetMonthsSalesHigherAverage() {
         int expected = 5;
-        int actual = service.getMonthsSalesHigherAverage(arr);
+        int actual = service.getMonthsSalesHigherAverage(sales);
         Assertions.assertEquals(expected, actual);
     }
 }
